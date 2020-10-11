7edaec05a01e3b0ed1c133cea0853085ce389453
liushaofei <liushaofei185@163.com>
2020-10-11 12:04:13 +0800

    商品列表组件的制作和添加商品的跳转

--- src/views/goods/list.vue
+++ src/views/goods/list.vue
@@ -1,9 +1,102 @@
 <template>
-  <div class="goods">商品列表</div>
+  <div class="goods">
+        <!-- 面包屑 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 搜索区域 -->
+    <div style="margin: 15px 0">
+      <el-input
+        placeholder="请输入内容"
+        v-model="goodsObj.query"
+        class="input-with-select"
+        style="width: 300px; margin-right: 10px"
+      >
+        <el-button
+          slot="append"
+          icon="el-icon-search"
+        ></el-button>
+      </el-input>
+      <el-button type="success" plain @click="$router.push({name: 'add'})">添加商品</el-button>
+    </div>
+    <!-- 表格数据展示 -->
+    <el-table :data="goodsList" style="width: 100%" border>
+      <el-table-column type="index" width="50"> </el-table-column>
+      <el-table-column prop="goods_name" label="商品名称" width="180"></el-table-column>
+      <el-table-column prop="goods_price" label="商品价格" width="180"> </el-table-column>
+      <el-table-column prop="goods_state" label="商品状态"> </el-table-column>
+      <el-table-column prop="goods_weight" label="商品重量"> </el-table-column>
+      <el-table-column prop="add_time" label="创建时间"> </el-table-column>
+      <!-- 添加操作列 -->
+      <el-table-column label="操作" width="220">
+        <template slot-scope="">
+          <el-tooltip class="item" effect="dark" content="编辑" placement="top">
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+            ></el-button>
+          </el-tooltip>
+          <el-tooltip class="item" effect="dark" content="删除" placement="top">
+            <el-button
+              type="primary"
+              icon="el-icon-delete"
+            ></el-button>
+          </el-tooltip>
+          <el-tooltip
+            class="item"
+            effect="dark"
+            content="审核"
+            placement="top"
+          >
+            <el-button type="primary" icon="el-icon-rank"></el-button>
+          </el-tooltip>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页 -->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="goodsObj.pagenum"
+      :page-sizes="[ 2, 4,6,8,10]"
+      :page-size="goodsObj.pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+    >
+    </el-pagination>
+  </div>
 </template>
 <script>
 export default {
-
+  data () {
+    return {
+      total: 100,
+      goodsObj: {
+        query: '',
+        pagesize: 10,
+        pagenum: 1
+      },
+      goodsList: []
+    }
+  },
+  methods: {
+    // 切换每页显示记录数触发
+    handleSizeChange (val) {
+      console.log(`每页 ${val} 条`)
+      // 修改全局的userObj.pagesize的值为val
+      this.goodsObj.pagesize = val
+      // 重新发送(修改后的userObj)请求,获取数据
+    },
+    // 切换当前页码触发
+    handleCurrentChange (val) {
+      console.log(`当前页: ${val}`)
+      // 修改全局的userObj.pagenum的值为val
+      this.goodsObj.pagenum = val
+      // 重新发送(修改后的userObj)请求,获取数据
+    }
+  }
 }
 </script>
 <style lang="less" scoped>
