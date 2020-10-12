40d568227ba4c565f3bba4e2960134fe233a358e
liushaofei <liushaofei185@163.com>
2020-10-12 09:30:52 +0800

    面包屑和步骤条的添加

--- src/views/goods/add.vue
+++ src/views/goods/add.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="goods">添加商品</div>
+  <div class="goods">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品添加</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 步骤条 -->
+    <el-steps :active="active" finish-status="success">
+      <el-step title="步骤 1"></el-step>
+      <el-step title="步骤 2"></el-step>
+      <el-step title="步骤 3"></el-step>
+      <el-step title="步骤 4"></el-step>
+      <el-step title="步骤 5"></el-step>
+    </el-steps>
+  </div>
 </template>
 <script>
 export default {
-
+  data () {
+    return {
+      active: 0
+    }
+  }
 }
 </script>
 <style lang="less" scoped>
-
 </style>
