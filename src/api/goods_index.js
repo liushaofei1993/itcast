8e6d6efcc9c5be1887dc29762acf63fa6ad2bc40
liushaofei <liushaofei185@163.com>
2020-10-12 09:03:00 +0800

    商品列表数据的动态展示及过滤器的添加

---
+++ src/api/goods_index.js
@@ -0,0 +1,11 @@
+// 这个文件用来处理所有和商品相关的操作
+// 引入axios
+import axios from '@/utils/myaxios.js'
+
+// 获取所有商品数据
+export const getAllGoodsList = (params) => {
+  return axios({
+    url: 'goods',
+    params
+  })
+}
